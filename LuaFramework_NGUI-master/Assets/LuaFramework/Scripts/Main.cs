25cc408c1924d7dd5c6b3b4263c64cfbaae14d48
abqipai <1157896317@qq.com>
2018-05-22 09:16:37 +0800

    ccc

--- LuaFramework_NGUI-master/Assets/LuaFramework/Scripts/Main.cs
+++ LuaFramework_NGUI-master/Assets/LuaFramework/Scripts/Main.cs
@@ -1,14 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace LuaFramework {
+namespace LuaFramework
+{
     /// <summary>
     /// 框架主入口
     /// </summary>
-    public class Main : MonoBehaviour {
+    public class Main : MonoBehaviour
+    {
 
-        void Start() {
-            AppFacade.Instance.StartUp();   //启动游戏
+        private UISlider slider;
+        private UILabel lable;
+        private GameObject root;
+        private GameObject gui;
+        // Use this for initialization
+        void Awake()
+        {
+            AppFacade.Instance.StartUp();
+            slider = GameObject.FindGameObjectWithTag("zairu").GetComponent<UISlider>();
+            lable = GameObject.FindGameObjectWithTag("lable1").GetComponent<UILabel>();
+            root = GameObject.Find("ROOT").gameObject;
+            slider.value = 0;
+            lable.text = "载入中...0 %";
+
         }
+        int a = 0;
+        // Update is called once per frame
+        void Update()
+        {
+
+            Ziru();
+        }
+        //场景加载
+        void Ziru()
+        {
+
+            a++;
+            if (a <= 180)
+            {
+
+                slider.value = a * 1.0f / 180;
+                lable.text = "载入中..." + System.Math.Ceiling(slider.value * 100) + "%";
+
+
+            }
+            else if (a > 210)
+            {
+                root.SetActive(false);
+
+                return;
+
+            }
+
+        }
+      
     }
 }
